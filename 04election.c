8cc01f05cd713c1b550017d40cefc7c328373520
sfaiz801 <sfaiz777@rediffemail.com>
2024-11-21 19:49:23 +0530

    My all c programing code

---
+++ 04election.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int number [5];
+    int odd;
+
+    printf("please enter any 5 number");
+
+     for(int i=0; i < 5; i++)
+     {
+         scanf("%d",&number [i]);
+
+     }
+
+    
+}
